ae6cf57f8d9ab0b2cf76d7bea78d3d98821826b7
Yoshifumi-Nakano <yoshifumi44230170@gmail.com>
2022-02-06 17:06:35 +0900

    retrive_transcript.py

--- retrieve_transcripts.py
+++ retrieve_transcripts.py
@@ -1,21 +1,29 @@
+# move LJSpeech transcription to raw_data/LJSpeech/LJSpeech
+# remove data which has number 
 import csv
 import os
+import shutil
 
-check=[]
-with open("LJSpeech/ljspeech/ljspeech/LJSpeech-1.1/metadata.csv", mode='r') as f:
+# change transcript_file_path and wav_dir_path into your ljspeech path
+transcript_file_path = "./ljspeech/LJSpeech-1.1/metadata.csv"
+wav_dir_path = "./ljspeech/LJSpeech-1.1/wavs"
+
+with open(transcript_file_path, mode='r') as f:
     reader = csv.reader(f,delimiter='\t')
-    reader=[r for r in reader]
+    reader = [r for r in reader]
+
+    if not os.path.exists("./raw_data/LJSpeech/LJSpeech/"):
+        os.makedirs("./raw_data/LJSpeech/LJSpeech/")
+
     for r in reader:
         filename=r[0].split("|")[0]
         text=r[0].split("|")[1]
-        
-        flg=any(t.isdigit() for t in text)
 
-        if flg:
-            os.remove("raw_data/LJSpeech/LJSpeech/"+filename+".wav")
-            check.append(filename)
-        else:
-            with open('raw_data/LJSpeech/LJSpeech/' + filename + '.lab', mode='w') as f:
+        number=any(t.isdigit() for t in text)
+
+        if not number:
+            shutil.move(os.path.join(wav_dir_path,filename+".wav"),"raw_data/LJSpeech/LJSpeech/")
+            with open(os.path.join("raw_data/LJSpeech/LJSpeech/" + filename + ".lab"), mode='w') as f:
                 f.write(text.strip('\n'))
-
-print(check)
+        else:
+            print(filename)
